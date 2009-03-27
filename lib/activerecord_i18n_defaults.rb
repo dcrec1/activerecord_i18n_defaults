6e301e78ef988316f04b96d0394b04ceb6afa227
Rafael Uchoa <uchoaaa@gmail.com>
2009-03-27 23:31:43 +0800

    Adapting to Rails 2.3.2
    Signed-off-by: Diego Carrion <dc.rec1@gmail.com>

--- lib/activerecord_i18n_defaults.rb
+++ lib/activerecord_i18n_defaults.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Base.class_eval do
 
    def self.human_attribute_name(attribute_key_name, options = {})
-     defaults = self_and_descendents_from_active_record.map do |klass|
+     defaults = self_and_descendants_from_active_record.map do |klass|
        :"#{klass.name.underscore}.#{attribute_key_name}"
      end
      defaults << "_all.#{attribute_key_name.to_s}".to_sym
